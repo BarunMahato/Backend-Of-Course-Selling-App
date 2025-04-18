23b03899b01ccbb0d99c9fbde98ed9c261364d91
Barun Mahato <mbarun515@gmail.com>
2025-04-18 11:32:09 +0530

    Created routes for user and course

---
+++ routes/course.js
@@ -0,0 +1,18 @@
+const {Router} = require('express');
+const courseRouter = Router();
+
+courseRouter.get("/purchase", function(req, res){
+    res.json({
+        message: "Courses on your purchase list."
+    })
+})
+
+courseRouter.post("/preview", function(req, res){
+    res.json({
+        message : "Courses you have bought and on the preview endpoint"
+    })
+})
+
+module.exports = {
+    courseRouter
+}
