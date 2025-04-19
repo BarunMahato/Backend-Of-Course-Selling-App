7baab81964fc65750253a5bd67443ce9e4011ea9
Barun Mahato <mbarun515@gmail.com>
2025-04-19 13:31:09 +0530

    Minor fixes in routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,22 @@
 const {Router} = require('express');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const JWT_SECRET  = process.env.JWT_SECRET;
 const adminRouter = Router();
 const {adminModel} = require('../db');
+
+function auth( req, res, next){
+    const token = req.headers.token;
+    const response = jwt.verify(token, JWT_SECRET);
+    if(response){
+        req.userId = response.userId;
+        next();
+    }else{
+        res.json({
+            message: "Sorry your userId did not match"
+        })
+    }
+}
 
 adminRouter.post("/signup", function(req, res){
     res.json({
